a6f6d2ad738989fad17753be7dc28e4de8d81219
Mario Quiros Araya <marioqa@gmail.com>
2017-03-18 14:38:45 -0600

    Generalizacion del scrip de reportes
    Utilizo el comando read para capturar la ruta a respaldar
    interactivamente
    
    Falta mejorar la forma de ingresar el path a respaldar
    
     Changes to be committed:
    	modified:   monitor.sh

--- src/monitor.sh
+++ src/monitor.sh
@@ -4,13 +4,21 @@
 # Ambos comandos son equivalentes
 current_pwd=`pwd`
 current_pwd=$(pwd)
-cd /usr/share/doc
+
+#read -p "Ingrese ruta a respaldar: " dir
+echo "Ingrese ruta a respaldar: "
+read dir
+
+#cd /usr/share/doc
 echo Reporte: >> /tmp/reporte
 date >> /tmp/reporte
-pwd >> /tmp/reporte
-du -sh . >> /tmp/reporte
+echo $dir >> /tmp/reporte
+
+#pwd >> /tmp/reporte
+du -sh $dir >> /tmp/reporte
 
 echo Path de ejecucion `pwd`
 
-cd $current_pwd
+#cd $current_pwd
 
+exit 0
