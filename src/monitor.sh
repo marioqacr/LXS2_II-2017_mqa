f0f5064ea92cd14d30bee0611507909fec8c408d
Mario Quiros Araya <marioqa@gmail.com>
2017-03-18 15:01:40 -0600

    Mejora del script de respaldo

--- src/monitor.sh
+++ src/monitor.sh
@@ -2,23 +2,13 @@
 # Programa para realizar respaldos de documentación
 
 # Ambos comandos son equivalentes
-current_pwd=`pwd`
-current_pwd=$(pwd)
 
-#read -p "Ingrese ruta a respaldar: " dir
-echo "Ingrese ruta a respaldar: "
-read dir
+read -p "Ingrese ruta a respaldar: " dir
 
-#cd /usr/share/doc
 echo Reporte: >> /tmp/reporte
 date >> /tmp/reporte
 echo $dir >> /tmp/reporte
 
-#pwd >> /tmp/reporte
 du -sh $dir >> /tmp/reporte
-
-echo Path de ejecucion `pwd`
-
-#cd $current_pwd
 
 exit 0
