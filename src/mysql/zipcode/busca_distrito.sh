7f69e114ad23a57ce72deccc93a649974d938285
Mario Quiros Araya <marioqa@gmail.com>
2017-03-26 00:44:19 -0600

    Cambios para hacer commit: 	nuevo archivo: busca_distrito.sh 	nuevo archivo: codigo_postal_cr.sql

---
+++ src/mysql/zipcode/busca_distrito.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+clear
+echo ""
+echo "********************************************************"
+echo -n "Introduce tu passwd de root de mysql: "
+stty -F $(tty) -echo
+read passwd
+stty -F$(tty) echo
+echo ""
+echo ""
+host="localhost"
+usuario="root"
+basedatos="zipcode"
+
+argumentos="-h $host -u $usuario -p$passwd -D $basedatos -s -e "
+while true
+do
+	read -p "Digite el nombre del distrito a buscar ( x para terminar): " distrito
+
+	if [ $distrito = 'x' ] 
+	then
+		exit 0
+	fi
+
+	campos='SELECT provincias.prnombre AS provincia, cantones.canombre AS canton, d.dinombre AS distrito, d.dicodigo AS codigo_postal '
+	tabla=" FROM distritos d "
+	joins=" JOIN cantones ON d.dicodcan =  cantones.cacodigo  JOIN provincias ON  d.dicodpro =  provincias.prcodigo "
+	filtro=" WHERE dinombre LIKE '%$distrito%';"
+	consulta="$campos  $tabla $joins $filtro"
+	
+
+	mysql $argumentos "SELECT provincias.prnombre AS provincia, cantones.canombre AS canton, d.dinombre AS distrito, d.dicodigo AS codigo_postal FROM distritos d JOIN cantones ON d.dicodcan =  cantones.cacodigo  JOIN provincias ON  d.dicodpro =  provincias.prcodigo WHERE dinombre LIKE '%$distrito%';"  2> /dev/null
+	echo "********************************************************"
+	echo ""
+	echo "" 
+done
